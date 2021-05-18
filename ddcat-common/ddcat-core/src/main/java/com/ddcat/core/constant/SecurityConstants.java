224cc5d8069e9550f1b2989cf27202224e25c317
张有良 <17639627374@163.com>
2021-05-18 16:04:42 +0800

    perf: pom文件包整合

--- ddcat-common/ddcat-core/src/main/java/com/ddcat/core/constant/SecurityConstants.java
+++ ddcat-common/ddcat-core/src/main/java/com/ddcat/core/constant/SecurityConstants.java
@@ -6,44 +6,9 @@ package com.ddcat.core.constant;
 public interface SecurityConstants {
 
     /**
-     * 角色前缀
-     */
-    String ROLE = "ROLE_";
-
-    /**
      * 项目的license
      */
     String PROJECT_LICENSE = "dd-cat";
-
-    /**
-     * 默认登录URL
-     */
-    String OAUTH_TOKEN_URL = "/oauth/token";
-
-    /**
-     * grant_type
-     */
-    String REFRESH_TOKEN = "refresh_token";
-
-    /**
-     * {bcrypt} 加密的特征码
-     */
-    String BCRYPT = "{bcrypt}";
-
-    /**
-     * 用户ID字段
-     */
-    String DETAILS_USER_ID = "user_id";
-
-    /**
-     * 用户名字段
-     */
-    String DETAILS_USERNAME = "username";
-
-    /**
-     * 用户部门字段
-     */
-    String DETAILS_DEPT_ID = "dept_id";
 
     /**
      * 协议字段
